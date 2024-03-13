6f037e44dc5e0f12b7aeb51ed517df457e5f6798
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2024-03-13 17:22:34 +0000

    add: testimonials

---
+++ src/Components/Testimonials/TestimonialCardList.tsx
@@ -0,0 +1,32 @@
+import UserAdmin from "../../Images/2203549_admin_avatar_human_login_user_icon.png";
+
+type Card = {
+  img: string;
+  name: string;
+  comment: string;
+  title: string;
+};
+
+export const TestimonialCardsList: Card[] = [
+  {
+    img: UserAdmin,
+    name: "Will Smith",
+    title: "Amazing App!",
+    comment:
+      "This app has truly revolutionized my fitness journey! From its user-friendly interface to its comprehensive workout plans and progress tracking features, I've never felt more motivated and empowered to achieve my fitness goals. Highly recommend to anyone looking to take their workouts to the next level!",
+  },
+  {
+    img: UserAdmin,
+    name: "Will Smith",
+    title: "Amazing App!",
+    comment:
+      "This app has truly revolutionized my fitness journey! From its user-friendly interface to its comprehensive workout plans and progress tracking features, I've never felt more motivated and empowered to achieve my fitness goals. Highly recommend to anyone looking to take their workouts to the next level!",
+  },
+  {
+    img: UserAdmin,
+    name: "Will Smith",
+    title: "Amazing App!",
+    comment:
+      "This app has truly revolutionized my fitness journey! From its user-friendly interface to its comprehensive workout plans and progress tracking features, I've never felt more motivated and empowered to achieve my fitness goals. Highly recommend to anyone looking to take their workouts to the next level!",
+  },
+];
---
+++ src/Components/Testimonials/Testimonials.tsx
@@ -0,0 +1,15 @@
+import Stars from "../../Images/5star.webp";
+import TestimonialCards from "./TestimonialCards";
+
+export default function Testimonials() {
+  return (
+    <section className="mt-5 px-[32px]">
+      <div className="text-white font-Roboto text-center flex justify-center items-center flex-col">
+        <img src={Stars} className="w-[200px]" />
+        <p className="font-semibold text-2xl">4.9/5.0</p>
+        <p className="font-semibold ">Overall Average Rating</p>
+      </div>
+      <TestimonialCards />
+    </section>
+  );
+}
